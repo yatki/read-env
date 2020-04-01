20c4907f51f5f044cdddefde713570de2c792877
yatki <mehmet@yatki.com>
2020-04-01 17:43:50 +0300

    Update rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require('./package.json');
@@ -19,14 +21,24 @@ const config = [
         format: 'esm',
         sourcemap: true,
       },
-      {
-        file: pkg.browser,
-        format: 'umd',
-        sourcemap: true,
-        name: 'readEnv',
-      },
     ],
-    plugins: [typescript(), terser({ output: { comments: false } })],
+    external: Object.keys(pkg.dependencies),
+    plugins: [typescript(), resolve(), terser({ output: { comments: false } })],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: true,
+      name: 'readEnv',
+    },
+    plugins: [
+      typescript(),
+      commonjs(),
+      resolve(),
+      terser({ output: { comments: false } }),
+    ],
   },
 ];
 
